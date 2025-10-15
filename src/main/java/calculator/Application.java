60747672041caf0d81b51b8273183762c889e6af
Seonggyun01 <thdrbs010314@naver.com>
2025-10-15 23:13:35 +0900

    feat: handle empty input

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,23 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 문자열 입력
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String inputString = Console.readLine();
+        int result = 0;
+
+        //입력 문자열이 null이나 공백인 경우
+        if (inputString == null || inputString.isEmpty()) {
+            System.out.println("결과 : " + result);
+            return;
+        }
+
+        System.out.println("결과 : " + result);
+        Console.close();
     }
 }
